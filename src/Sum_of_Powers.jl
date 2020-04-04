a3cdff19df5cd90efcb8f306a26523b75513e722
Christopher Stook <chris@chrisstook.com>
2020-04-04 19:46:25 -0400

    Files generated by PkgTemplates

---
+++ src/Sum_of_Powers.jl
@@ -0,0 +1,5 @@
+module Sum_of_Powers
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Sum_of_Powers
+using Test
+
+@testset "Sum_of_Powers.jl" begin
+    # Write your own tests here.
+end
