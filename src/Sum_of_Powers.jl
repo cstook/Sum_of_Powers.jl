f7f2586cdfbeae5ff550b6a866cf1f5a9a1cf084
Christopher Stook <chris@chrisstook.com>
2020-04-06 08:58:45 -0400

    gfd

--- src/Sum_of_Powers.jl
+++ src/Sum_of_Powers.jl
@@ -1,5 +1,23 @@
 module Sum_of_Powers
 using Primes
-greet() = print("Hello World!")
+import Base.string
+
+struct Solution{T<:Integer,N}
+    s :: T
+    n :: Int
+    a :: NTuple{N,T}
+end
+
+function Base.string(s::Solution)
+    out = string(s.s,"^",s.n,"=>{",s.a[1])
+    for value in s.a[2:end]
+        out = out*string(",",value)
+    end
+    out = out*"}"
+    out
+end
+
+s = Solution(15,4,(14,9,8,6,3))
+string(s)
 
 end # module
