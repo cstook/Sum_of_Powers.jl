f7dba6398ab2a71f2184184cfa64f27aebcd1d19
Christopher Stook <chris@chrisstook.com>
2020-04-13 19:25:49 -0400

    probably an overflow somewhere...

--- src/Sum_of_Powers.jl
+++ src/Sum_of_Powers.jl
@@ -1,9 +1,11 @@
 module Sum_of_Powers
 import Base.string
 import Combinatorics.combinations
-using Plots
+# using Plots
 
-export Solution, err, best, print_best, print_best_to_file, max_all_a,random_search
+export Solution, err, best, print_best, print_best_to_file,
+       max_all_a,random_search, best_group!, improve!,solution,
+       Strategy
 
 #=
 struct Solution{T<:Integer,N}
@@ -12,11 +14,33 @@ struct Solution{T<:Integer,N}
     a :: NTuple{N,T}
 end
 =#
+struct Strategy{T}
+    groups :: Vector{T}
+end
 
-struct Solution{T<:Integer}
+mutable struct Solution{T<:Integer}
     s :: T
     n :: Int
     a :: Array{T,1}
+end
+solution(s,n) = Solution{Int}(s,n,[])
+function canonical!(solution::Solution)
+    sort!(solution.a)
+end
+function isvalid(solution::Solution)
+    s = solution.s
+    n = solution.n
+    a = solution.a
+    s<3 && return false
+    n<16 && return false
+    n>40 && return false
+    length(a)<2 && return false
+    length(a)>(s-1) && return false
+    for x in a
+        x<1 && return false
+        x>(s-1) && return false
+    end
+    true
 end
 
 function Base.string(sol::Solution)
@@ -38,7 +62,7 @@ end
 
 function random_search(s, n, iterations=1e5,
                              best_e=s^(BigInt(n)),
-                             powers=powers_tuple(s,n))
+                             powers=powers_tuple_old(s,n))
     s_to_n = s^(BigInt(n))
     best_a = Vector{Int}()
     all_a = Set(1:s-1)
@@ -66,24 +90,70 @@ function random_search(s, n, iterations=1e5,
         end
     end
     if gotone
-        return Solution(sort(s,rev=true),n,best_a),best_e
+        return Solution(s,n,sort(best_a,rev=true)),best_e
     else
         return nothing
     end
 end
 
-function improve!(sol::Solution)
-    e = err(sol)
-    if e<1 return 1 end
-    x = round(abs(e)^(1/sol.n))
-    newa = rand(1:x-1)
-    if newa in sol.a return 2 end
-    push!(sol.a,newa)
-    return 3
+function improve!(solution::Solution, strategy::Strategy;
+                  powers=powers_tuple(solution.s,solution.n),
+                  initial_error=err(solution),
+                  s_to_n=solution.s^(BigInt(solution.n))
+                  )
+    current_error = BigInt(initial_error)
+    new_error = BigInt(initial_error)
+    current_solution = solution
+    while true
+        for group in strategy.groups
+            new_error = best_group!(current_solution, group,
+                                    powers=powers,
+                                    initial_error=current_error,
+                                    s_to_n = s_to_n)
+        end
+        new_error>=current_error && break
+        current_error = new_error
+    end
+    solution = current_solution
+    current_error
 end
 
-powers_tuple(s,n) = ntuple(x->(s-x)^BigInt(n),s-1)
-function best(s,n,powers=powers_tuple(s,n))
+function best_group!(solution::Solution, group;
+                     powers=powers_tuple(solution.s,solution.n),
+                     initial_error=err(solution),
+                     s_to_n=solution.s^(BigInt(solution.n))
+                    )
+    s = solution.s
+    n = solution.n
+    a = solution.a
+    lhs = BigInt(s_to_n)
+    a_not_in_group = setdiff(solution.a,group)
+    for i in a_not_in_group
+        lhs -= powers[i]
+    end
+    best_e = BigInt(initial_error)
+    best_c = Array{Int,1}[]
+    for c in combinations(group)
+        e = BigInt(lhs)
+        for i in c
+            e=e-powers[i]
+            e<0 && abs(e)>abs(best_e) && break
+        end
+        if abs(e)<=abs(best_e)
+            best_e = e
+            best_c = c
+        end
+        best_e==0 && break
+    end
+    best_a = sort!(union(best_c,a_not_in_group))
+    solution.a = best_a
+    best_e
+end
+
+
+powers_tuple(s,n) = ntuple(x->x^BigInt(n),s-1)
+powers_tuple_old(s,n) = ntuple(x->(s-x)^BigInt(n),s-1)
+function best(s,n,powers=powers_tuple_old(s,n))
     s_to_n = s^(BigInt(n))
     best_e = s_to_n
     all = collect(s-1:-1:1)
@@ -115,7 +185,7 @@ end
 function max_all_a(n)
     s=2
     while true
-        powers=powers_tuple(s,n)
+        powers=powers_tuple_old(s,n)
         all = collect(s-1:-1:1)
         e = s^(BigInt(n))
         for i in all
@@ -128,7 +198,7 @@ function max_all_a(n)
     end
 end
 
-function print_best(io::IO,s,n,powers=powers_tuple(s,n))
+function print_best(io::IO,s,n,powers=powers_tuple_old(s,n))
     sol,e = best(s,n,powers)
     println(io,string(sol),",e=",string(e))
 end
