b8ff469029080de653f8fe66ba9a56f2196f45e0
Christopher Stook <chris@chrisstook.com>
2020-04-19 12:05:42 -0400

    search Best

--- src/Sum_of_Powers.jl
+++ src/Sum_of_Powers.jl
@@ -1,11 +1,12 @@
 module Sum_of_Powers
 export Solution, err
+export search, Best
 
 
 import Base.string
 
 include("Solution.jl")
 include("OnePositions.jl")
-include("solve.jl")
+include("search.jl")
 
 end  #module
