4e9e943bfc2d3273c35db6731532267a62da45c5
Christopher Stook <chris@chrisstook.com>
2020-05-04 14:59:23 -0400

    aa

--- src/look_up_table.jl
+++ src/look_up_table.jl
@@ -20,7 +20,74 @@ function problem_terms(max_k, n, to_n=a_to_n(max_k,n), ctn=cumulative_a_to_n(max
     end
     x, y
 end
-function drop_terms(max_k, n, to_n=a_to_n(max_k,n))
+
+
+# find the extent of the overlap around k where the binary representation of the a_k's is
+# not in order.  Assume no double overlap for now.
+function overlap_limits(max_k, k, n,
+                        tn=a_to_n(max_k,n),
+                        ctn=cumulative_a_to_n(max_k, n, tn))
+    # for lack of better names we will use b,c,d,e
+    # b,e will be the limits of the overlap to be found by this function
+    # c,d are easy to understand from the code below
+    # when we are done b<c<d<e and b,e are the limits of overlap around term k
+    c = tn[k]
+    d = ctn[k-1]
+    c>=d && return nothing # no overlap
+    # find b
+    search_range_min = (k-1)>>1
+    search_range_max = k-1
+    target_value = c
+    b = binary_search(max_k,
+                     rhs_b_min, rhs_b_max,
+                     target_value,
+                     n,tn)
+    # find e
+    search_range_min = k
+    search_range_max = k<<1
+    target_value = d
+    e = binary_search(max_k,
+                     rhs_b_min, rhs_b_max,
+                     target_value,
+                     n,tn) +1
+    b,e
+end
+function binary_search(max_k,
+                      rhs_b_min, rhs_b_max,
+                      target_value::BigInt,
+                      n,
+                      tn=a_to_n(max_k,n))
+    # return binary representation of rhs adjacent to target value.
+    # it will be the rhs below the target value.
+    # assumes the rhs value is in the same order as its binary representation.
+    rhs_b = BigInt(0)
+    previous_rhs_b = BigInt(0)
+    rhs_v = BigInt(0)
+    e = BigInt(0)
+    while true
+        previous_rhs_b = rhs_b
+        rhs_b = div(rhs_b_min+rhs_b_max,2)
+        previous_rhs_b == rhs_b && break
+        rhs_v = sum(tn[OnePositions(rhs_b)])
+        e = target_value - rhs_v
+        e == 0 && break
+        e>0 ? (rhs_b_min=rhs_b) : (rhs_b_max=rhs_b)
+    end
+    if e>0 && iseven(rhs_b)
+        rhs_b+=1
+        e-=1
+    end
+    return rhs_b
+end
+
+function first_problem_term(max_k, n, to_n=a_to_n(max_k,n), ctn=cumulative_a_to_n(max_k, n, to_n))
+    for k in 2:max_k
+        e = to_n[k]-ctn[k-1]
+        e<0 && return k # is kth term less than the sum of all previous terms
+    end
+    nothing
+end
+function drop_terms(max_k, n, to_n=a_to_n(max_k,n)) # this is wrong
     dt = Vector{Int}() # dropped terms
     it = Vector{Int}([1]) # included terms
     cumulative_included_terms = Vector{BigInt}([to_n[1]])
